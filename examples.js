74f52e72513a618550569c8bccd3db950844839e
Stelian Rosca <stelianro>
2023-03-29 21:04:03 +0300

    display none page menu

---
+++ examples.js
@@ -0,0 +1,15 @@
+console.in;
+var age = 28;
+var name = "St";
+
+var pers = {
+  age: 25,
+  name: "St",
+  skills: ["html", "js"],
+  learn: function () {
+    return "I'm learning...";
+  },
+};
+console.log(person.name);
+var l = person.learn;
+console.warn(l);

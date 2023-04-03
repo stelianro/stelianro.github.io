759a6218ded2440b5fcc23650fe49f7c445a1df2
Stelian Rosca <stelianro>
2023-04-03 17:32:44 +0300

    implemented show functions

--- index.js
+++ index.js
@@ -3,8 +3,30 @@ var activePage = "home";
 var homePage = document.getElementById(activePage);
 homePage.style.display = "block";
 
+function showHome() {
+  document.getElementById("home").style.display = "block";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+}
+
 function showSkills() {
-  console.warn("te rog afiseaza skills");
   document.getElementById("home").style.display = "none";
   document.getElementById("skills").style.display = "block";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+}
+
+function showProjects() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "block";
+  document.getElementById("languages").style.display = "none";
+}
+
+function showLanguages() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "block";
 }
